3b63bff8c9ea6e918fdc6939404925c7c738e192
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2022-03-20 17:06:36 -0300

    add simple error capture in getImage

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import Model from "./model.js"
 // used a "self-invoking" function because it is not allowed to use "await" in top level scripts
 (async function() {
   window.opa = new Model()
-  var x = await window.opa.getImage("https://100ze.github.io/lydia/img/lydia.jpg")
+  var x = await window.opa.getImage("https://100ze.github.io/lydia/img/lydiaaaaa.jpg")
   console.log(x)
   document.querySelector("img").src = x
   console.log("it's me mario!")
--- app/model.js
+++ app/model.js
@@ -4,9 +4,11 @@ export default class Model {
   // function to download images and return as a url that can be used on the page
   async getImage(url) {
     return await fetch(url)
-      .then(response => response.blob())
+      .then(response => {
+        return response.ok ? response.blob() : false
+      })
       .then(response_blob => {
-        return URL.createObjectURL(response_blob)
+        return response_blob ? URL.createObjectURL(response_blob) : false
       })
   }
 } 
